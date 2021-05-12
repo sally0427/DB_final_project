d84287e7e3e6f6541a3d1b659de01d4aa5f228f3
sally0427 <a29853602@gmail.com>
2021-05-12 19:10:07 +0800

    change_settings

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from store import views
+from uber_eat import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
