bc7bbfaca913e4e9e2bb20936acf50fe5a7f6993
sally0427 <a29853602@gmail.com>
2021-05-15 16:55:58 +0800

    add_del

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -20,16 +20,20 @@ from uber_eat import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/', views.test, name ='test'),
-    path('add_store_get/', views.add_store_get, name ='add_store_get'),
     path('add_store/', views.add_store, name ='add_store'),
-    path('add_product_get/', views.add_product_get, name ='add_product_get'),
+    path('add_store_post/', views.add_store_post, name ='add_store_post'),
     path('add_product/', views.add_product, name ='add_product'),
-    path('add_consumer_get/', views.add_consumer_get, name ='add_consumer_get'),
+    path('add_product_post/', views.add_product_post, name ='add_product_post'),
     path('add_consumer/', views.add_consumer, name ='add_consumer'),
-    path('add_deliver_get/', views.add_deliver_get, name ='add_deliver_get'),
+    path('add_consumer_post/', views.add_consumer_post, name ='add_consumer_post'),
     path('add_deliver/', views.add_deliver, name ='add_deliver'),
-    path('add_order/', views.add_order, name ='add_order'),
-    path('add_ordergoods/', views.add_ordergoods, name ='add_ordergoods'),
-    
-    
+    path('add_deliver_post/', views.add_deliver_post, name ='add_deliver_post'),
+    path('add_order_post/', views.add_order_post, name ='add_order_post'),
+    path('add_ordergoods_post/', views.add_ordergoods_post, name ='add_ordergoods_post'),
+    path('del_store/', views.del_store, name ='del_store'),
+    path('del_store_post/', views.del_store_post, name ='del_store_post'),   
+    path('del_product/', views.del_product, name ='del_product'),
+    path('del_product_post/', views.del_product_post, name ='del_product_post'),
+    path('del_consumer/', views.del_consumer, name ='del_consumer'),
+    path('del_consumer_post/', views.del_consumer_post, name ='del_consumer_post'),  
 ]
--- uber_eat/views.py
+++ uber_eat/views.py
@@ -6,43 +6,62 @@ def test(request):
     return render(request, "index.html")
 
 
-def add_store_get(request):
+def add_store(request):
     return render(request, "add_store.html")
 
-def add_store(request):
+def add_store_post(request):
     addStore = Store(id = request.POST['id'], address = request.POST['address'], name = request.POST['name'], phone = request.POST['phone'], transit_price = request.POST['transit_price']).save()
     return HttpResponse('<p>Add store</p>')
 
-def add_product_get(request):
+def add_product(request):
     return render(request, "add_product.html")
 
-def add_product(request):
+def add_product_post(request):
     addProduct = Product(id = request.POST['id'], store_id = request.POST['store_id'], name = request.POST['name'], price = request.POST['price']).save()
     return HttpResponse('<p>Add product</p>')
 
-def add_consumer_get(request):
+def add_consumer(request):
     return render(request, "add_consumer.html")
 
-def add_consumer(request):
+def add_consumer_post(request):
     addConsumer = Consumer(id = request.POST['id'], address = request.POST['address'], name = request.POST['name'], phone = request.POST['phone']).save()
     return HttpResponse('<p>Add consumer</p>')
 
-def add_deliver_get(request):
+def add_deliver(request):
     return render(request, "add_deliver.html")
 
-def add_deliver(request):
+def add_deliver_post(request):
     addDeliver = Deliver(id = request.POST['id'], name = request.POST['name'], phone = request.POST['phone']).save()
     return HttpResponse('<p>Add deliver</p>')
 
-def add_order(self):
+def add_order_post(self):
     addOrder = Order.objects.create(id = 1, consumer_id = 1, store_id = 1)
     addOrder = Order(id = 1, consumer_id = 1, store_id = 1).save()
     return HttpResponse('<p>Add Order</p>')
 
-def add_ordergoods(self):
+def add_ordergoods_post(self):
     # id = 1, order_id = 1, product_id = 1
     addOrdergoods = OrderGoods.objects.create(id = 2, order_id = 1, product_id = 2, count = 2)
     addOrdergoods = OrderGoods(id = 2, order_id = 1, product_id = 2, count = 2).save()
     return HttpResponse('<p>Add OrderGoods</p>')
 
+def del_store(request):
+    return render(request, "del_store.html")
 
+def del_store_post(request):
+    delProduct = Store.objects.filter(id = request.POST['id']).delete()
+    return HttpResponse('<p>Del store</p>')
+
+def del_product(request):
+    return render(request, "del_product.html")
+
+def del_product_post(request):
+    delProduct = Product.objects.filter(id = request.POST['id']).delete()
+    return HttpResponse('<p>Del product</p>')
+
+def del_consumer(request):
+    return render(request, "del_consumer.html")
+
+def del_consumer_post(request):
+    delConsumer = Consumer.objects.filter(id = request.POST['id']).delete()
+    return HttpResponse('<p>Del consumer</p>')
