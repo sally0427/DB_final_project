c54032491f4dfa176bf25afa9ffb4b4a7d6288db
guanlin-chen08 <m10902147@gapps.ntust.edu.tw>
2021-05-24 14:46:13 +0800

    add_sally_commit

--- uber_store/views.py
+++ uber_store/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from uber_store import forms
+from uber_eat.models import Store
+
 
 # Create your views here.
+
+def home(request):
+    form = forms.SignUpForm
+    return render(request, 'store/storeRegistration.html', locals())
+
+
+def add_store_post(request):
+    Store.objects.create(Sname='test', Saddress='At Earth', Sphone='123456789')
+    return home()
