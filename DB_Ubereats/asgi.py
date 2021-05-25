08e1067bb3328659f32ef2125f6f608ad95db804
guanlin-chen08 <m10902147@gapps.ntust.edu.tw>
2021-05-25 15:51:01 +0800

    help_commit

--- DB_Ubereats/asgi.py
+++ DB_Ubereats/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DB_Ubereats.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DB_Ubereats.settings.py')
 
 application = get_asgi_application()
